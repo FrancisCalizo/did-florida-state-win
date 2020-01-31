f1da603581bb7be6a8119a498ceef5c2211d34d8
Francis Calizo <francisacalizo@gmail.com>
2020-01-31 10:09:27 -0500

    Add Garnet/Gold shading properties to tailwind.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h1 className="bg-red-100">
+      <h1 className="bg-garnet-500">
         This page should show the past game, current game, and/or next game
       </h1>
-      <h2>You still need to find an API for this big dawg</h2>
+      <h2 className="bg-gold-500">
+        You still need to find an API for this big dawg
+      </h2>
       <h3>
         {/* Change 2020 to dynamic date */}
         Check out past games <Link to="/Schedule/2020">Here</Link>
