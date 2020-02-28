95b0172c98b089d5d1a0cdf62c7ebf91fd98411c
Francis Calizo <francisacalizo@gmail.com>
2020-02-28 16:48:56 -0500

    create roster column space and responsive

--- src/components/players/RosterItem.js
+++ src/components/players/RosterItem.js
@@ -25,36 +25,35 @@ const RosterItem = ({ roster }) => {
     <div className="flex flex-wrap my-8 mx-8">
       {roster.map(player => {
         return (
-          <div
-            key={player.id}
-            className="w-full lg:w-1/2 my-1 py-2 border border-dark-200 rounded"
-          >
-            <div className="flex">
-              <img
-                className="w-32 inline-block mx-4"
-                src={require('../../images/avatar.png')}
-                alt="avatar"
-              />
-              <div className="flex items-center mx-2">
-                <div>
-                  <h2>
-                    {player.position} /{' '}
-                    {`${parseInt(player.height / 12)}'${parseInt(
-                      player.height % 12
-                    )}"`}
-                    -{player.weight} |{' '}
-                    <span className="underline font-bold">
-                      {getClass(player.year)}
-                    </span>
-                  </h2>
-                  <h1 className="text-2xl my-2">
-                    <span className="border-2 px-2 bg-dark-800 text-white">
-                      #{player.jersey}
-                    </span>
-                    {'  '}
-                    {`${player.first_name} ${player.last_name}`}
-                  </h1>
-                  <h2 className="text-sm">{`${player.home_city}, ${player.home_state}`}</h2>
+          <div key={player.id} className="w-full lg:w-1/2">
+            <div className="my-1 lg:mx-1 py-4 border border-dark-200 rounded">
+              <div className="flex">
+                <img
+                  className="w-32 inline-block mx-4"
+                  src={require('../../images/avatar.png')}
+                  alt="avatar"
+                />
+                <div className="flex items-center mx-2">
+                  <div>
+                    <h2>
+                      {player.position} /{' '}
+                      {`${parseInt(player.height / 12)}'${parseInt(
+                        player.height % 12
+                      )}"`}
+                      -{player.weight} |{' '}
+                      <span className="underline font-bold">
+                        {getClass(player.year)}
+                      </span>
+                    </h2>
+                    <h1 className="text-2xl my-2">
+                      <span className="border-2 px-2 bg-dark-800 text-white">
+                        #{player.jersey}
+                      </span>
+                      {'  '}
+                      {`${player.first_name} ${player.last_name}`}
+                    </h1>
+                    <h2 className="text-sm">{`${player.home_city}, ${player.home_state}`}</h2>
+                  </div>
                 </div>
               </div>
             </div>
