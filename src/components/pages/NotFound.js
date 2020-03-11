f70ba2ebaf68d6a3b34b07fc9f6396280b447922
Francis Calizo <francisacalizo@gmail.com>
2020-03-10 23:17:40 -0400

    About Styling

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -5,12 +5,13 @@ const NotFound = () => {
     <div className="flex justify-center">
       <div className="w-1/3">
         <div className="text-center">
-          <h1 className="text-8xl font-bold">404</h1>
+          <h1 className="text-8xl font-bold">404!</h1>
+          <h3>We can't seem to find the page you're looking for.</h3>
         </div>
       </div>
       <div className="w-1/3">
         <img
-          className="w-96"
+          className="block w-96 mx-auto"
           src={require('../../images/seminole.png')}
           alt="seminole-404"
         />
