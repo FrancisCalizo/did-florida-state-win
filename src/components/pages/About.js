de321d63ef6a76addf16f464093480a134c70251
Francis Calizo <francisacalizo@gmail.com>
2020-03-11 16:48:40 -0400

    Restructure about apge

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="-mt-2 mb-10 h-screen">
-      <div className="bg-fixed bg-chief h-112">
+    <div className="-mt-2">
+      <div className="bg-fixed bg-chief">
         <div className="flex justify-center items-center h-full">
-          <div>
-            <h1 className="text-white font-bold text-5xl">Hello There.</h1>
+          <div className="py-48">
+            <h1 className="text-white font-bold text-5xl">
+              Florida State Football
+            </h1>
+            <h3 className="text-white text-xl text-center">
+              Scores | Updates | Roster
+            </h3>
           </div>
         </div>
       </div>
