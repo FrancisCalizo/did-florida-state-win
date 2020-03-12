2c0498ef732d44ca0e63d876bc703cdbe316fa3d
Francis Calizo <francisacalizo@gmail.com>
2020-03-12 00:24:59 -0400

    More styling of about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,28 +3,34 @@ import React, { useState } from 'react';
 const About = () => {
   const [card, setCard] = useState([
     {
+      id: 1,
       title: 'Updates',
       body: 'Get updates on if your beloved noles won or miserably lost.'
     },
     {
+      id: 2,
       title: 'Schedules',
       body: 'View future and past schedules by year.'
     },
     {
+      id: 3,
       title: 'Game Details',
       body:
         'Box scores, team stats, and play-by-play information for each game.'
     },
     {
+      id: 4,
       title: 'Roster',
       body: 'View rosters by year.'
     },
     {
+      id: 5,
       title: 'Countdown',
       body:
         'See when the next game is being played by your noles with an official gameday countdown.'
     },
     {
+      id: 6,
       title: 'Code',
       body:
         'See the code for this website by checking out the GitHub Icon on the top right of the footer.'
@@ -36,23 +42,25 @@ const About = () => {
       <div className="bg-fixed bg-chief border-b-8 border-black">
         <div className="flex justify-center items-center h-full">
           <div className="py-48">
-            <h1 className="text-white font-bold text-5xl">
+            <h1 className="text-white font-bold text-4xl md:text-5xl">
               Did Florida State Win?
             </h1>
-            <h3 className="text-white text-xl text-center">
+            <h3 className="text-white text-lg md:text-xl text-center">
               Scores | Updates | Roster
             </h3>
           </div>
         </div>
       </div>
-      <div>
-        <h2 className="text-center font-bold text-4xl">
-          Provided for the fans of Florida State Football:
-        </h2>
-        <div className="flex flex-wrap justify-center">
+      <div className=" mb-24">
+        <div className="flex justify-center">
+          <h2 className="uppercase inline-block text-center font-bold mt-8 mb-6 bg-gold-500 px-8 py-1 rounded text-lg sm:text-2xl md:text-4xl border-2 border-black">
+            For the fans of Florida State Football
+          </h2>
+        </div>
+        <div className="flex flex-wrap justify-center mx-6">
           {card.map(info => {
             return (
-              <div className="w-1/3 h-48 my-4">
+              <div className="w-full sm:w-1/2 lg:w-1/3 h-48 my-4" key={info.id}>
                 <div
                   className="text-center rounded border border-dark-200
               shadow-2xl bg-white h-full px-10 mx-6"
