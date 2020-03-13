8d40ab626779f12ff98800bf0250d711bbf454e2
Francis Calizo <francisacalizo@gmail.com>
2020-03-12 23:04:43 -0400

    https fixes

--- src/components/layout/RosterDropdown.js
+++ src/components/layout/RosterDropdown.js
@@ -35,7 +35,7 @@ const RosterDropdown = () => {
         <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center text-gray-700">
           <svg
             className="fill-current h-8 w-8 text-black"
-            xmlns="http://www.w3.org/2000/svg"
+            xmlns="https://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
           >
             <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
